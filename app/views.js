30d660116ade12bc34ceae9febfb4db412a41057
mshgh <36337240+mshgh@users.noreply.github.com>
2019-05-26 15:01:30 +0200

    Added ShowState & some styling

--- app/views.js
+++ app/views.js
@@ -1,14 +1,15 @@
 import { h } from './3pty/hyperapp.js'
 
 const App = state =>
-  h('div', {}, [
+  h('div', { style: { 'font-family': 'Verdana, Geneva, Tahoma, sans-serif' } }, [
     h('div', { style: { float: 'left', width: '50%' } }, 
-      h(Typeahead, { ...state.author, label: 'Search for an author:' })
+      h(Typeahead, { ...state.author, label: 'Search an author:' })
     ),
     h('div', { style: { float: 'left', width: '50%' } }, 
-      h(Typeahead, { ...state.book, label: 'Search for a book:' })
+      h(Typeahead, { ...state.book, label: 'Search a book:' })
     ),
-    h('div', { style: { clear: 'both' } })
+    h('div', { style: { clear: 'both' } }),
+    h(ShowState, state)
   ])
 
 // --- components ---
@@ -20,6 +21,12 @@ const Typeahead = ({ label, value, suggestions = [] }) =>
   ]
 const TypeaheadSuggestions = ({ suggestions }) => h('ul', {}, suggestions.map(suggestion => h(TypeaheadSuggestion, { ...suggestion })))
 const TypeaheadSuggestion = ({ id, displayText }) => h('li', { id }, displayText)
+
+const ShowState = state =>
+  h('div', { style: { border: 'black 1px solid', padding: '0.2em', 'margin-top': '0.5em' } }, [
+    h('h3', { style: { margin: '0.2em', 'border-bottom': 'black 1px solid' } }, 'State'),
+    h('pre', { style: { margin: '0.2em' } }, JSON.stringify(state, null, 2))
+  ])
 
 export default {
   App
