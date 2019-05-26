e8014b49ee8b0aa4419198cf50345f83f7ba76a1
mshgh <36337240+mshgh@users.noreply.github.com>
2019-05-26 14:46:33 +0200

    Two typeaheads w/hard-coded state

--- app/actions.js
+++ app/actions.js
@@ -1,5 +1,25 @@
-const init = () => 0
+const init = () => ({
+  author: {
+    value: 'a',
+    limit: 10,
+    suggestions: [
+      { id: 1, displayText: 'Frank Herbert' },
+      { id: 2, displayText: 'Dan Simmmons' },
+      { id: 3, displayText: 'Terry Pratchett' }
+    ]
+  },
+  book: {
+    value: 'e',
+    limit: 10,
+    suggestions: [
+      { id: 4, displayText: 'Dune' },
+      { id: 5, displayText: 'Hyperion' },
+      { id: 6, displayText: 'Maskerade' }
+    ]
+  }
+})
 
 export default {
   init
 }
+// https://openlibrary.org/search/authors.json?q=herbert&limit=1&mode=ebooks
--- app/views.js
+++ app/views.js
@@ -1,11 +1,25 @@
 import { h } from './3pty/hyperapp.js'
 
 const App = state =>
-  h("div", {}, [
-    h("h1", {}, state),
-    h("button", { onclick: state => state - 1 }, "-"),
-    h("button", { onclick: state => state + 1 }, "+")
+  h('div', {}, [
+    h('div', { style: { float: 'left', width: '50%' } }, 
+      h(Typeahead, { ...state.author, label: 'Search for an author:' })
+    ),
+    h('div', { style: { float: 'left', width: '50%' } }, 
+      h(Typeahead, { ...state.book, label: 'Search for a book:' })
+    ),
+    h('div', { style: { clear: 'both' } })
   ])
+
+// --- components ---
+const Typeahead = ({ label, value, suggestions = [] }) =>
+  [
+    h('h4', {}, label),
+    h('input', { value }),
+    h(TypeaheadSuggestions, { suggestions })
+  ]
+const TypeaheadSuggestions = ({ suggestions }) => h('ul', {}, suggestions.map(suggestion => h(TypeaheadSuggestion, { ...suggestion })))
+const TypeaheadSuggestion = ({ id, displayText }) => h('li', { id }, displayText)
 
 export default {
   App
