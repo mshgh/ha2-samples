7be2ef56527a8814a7cfb51cb39f73c165fa8355
mshgh <36337240+mshgh@users.noreply.github.com>
2019-12-09 14:04:21 +0100

    Cleaning implementation
     - more reliable way to destructure modules

--- app/app-components.js
+++ app/app-components.js
@@ -1,16 +1,22 @@
 import { h } from 'https://unpkg.com/hyperapp@2.0.3/src/index.js'
 import { ShowState } from './components/show-state.js'
+import { indexes as module } from './app-modules.js'
 
-export const Page = ({ state, title, actions: [a, b, c, d, e], views: [aViews, bViews, cViews, dViews, eViews] }) => h('body', { style: { 'font-family': 'sans-serif' } },
-  h('h2', {}, title),
-  aViews.IncDec({ title: 'Counter A', incrementOther: b.increment, decrementOther: b.decrement }),
-  bViews.IncDec({ title: 'Counter B', incrementOther: c.increment, decrementOther: c.decrement }),
-  cViews.IncDec({ title: 'Counter C', incrementOther: d.increment, decrementOther: d.decrement }),
-  dViews.IncDec({ title: 'Counter D', incrementOther: e.increment, decrementOther: e.decrement }),
-  eViews.IncDec({ title: 'Counter E', incrementOther: a.increment, decrementOther: a.decrement }),
-  h('hr'),
-  cViews.Settings(),
-  eViews.Settings(),
-  h('hr'),
-  ShowState({ state, indent: 3 })
-)
+export const Page = ({
+  state, title,
+  actions: { [module.a]: a, [module.b]: b, [module.c]: c, [module.d]: d, [module.e]: e },
+  views: { [module.a]: aViews, [module.b]: bViews, [module.c]: cViews, [module.d]: dViews, [module.e]: eViews }
+}) =>
+  h('body', { style: { 'font-family': 'sans-serif' } },
+    h('h2', {}, title),
+    aViews.IncDec({ title: 'Counter A', incrementOther: b.increment, decrementOther: b.decrement }),
+    bViews.IncDec({ title: 'Counter B', incrementOther: c.increment, decrementOther: c.decrement }),
+    cViews.IncDec({ title: 'Counter C', incrementOther: d.increment, decrementOther: d.decrement }),
+    dViews.IncDec({ title: 'Counter D', incrementOther: e.increment, decrementOther: e.decrement }),
+    eViews.IncDec({ title: 'Counter E', incrementOther: a.increment, decrementOther: a.decrement }),
+    h('hr'),
+    cViews.Settings(),
+    eViews.Settings(),
+    h('hr'),
+    ShowState({ state, indent: 3 })
+  )
--- app/app-modules.js
+++ app/app-modules.js
@@ -1,19 +1,16 @@
-import { ns, init as modulesInit } from './modules.js'
+import { ns } from './modules.js'
 import Counter from './modules/counter/index.js'
 import PositiveCounter from './modules/positive-counter/index.js'
 
+const all = []
+const addModule = (Module, slice, props, ns = { add: (Module, slice, props) => Module(slice, props) }) => all.push({ slice, module: ns.add(Module, slice, props) })
+
 const counters = ns('foo').add(ns, 'bar').add(ns, 'counters')
+addModule(Counter, 'a', { name: 'A' })
+addModule(Counter, 'b', { name: 'B', count: 3 })
+addModule(PositiveCounter, 'c', { name: 'C', count: 5 })
+addModule(Counter, 'd', { name: 'D', count: 7 }, counters)
+addModule(PositiveCounter, 'e', { name: 'E', count: 13, positive: true }, counters)
 
-export const modules = [
-  Counter('a', { name: 'A' }),
-  Counter('b', { name: 'B', count: 3 }),
-  PositiveCounter('c', { name: 'C', count: 5, positive: false }),
-  counters.add(Counter, 'd', { name: 'D', count: 7 }),
-  counters.add(PositiveCounter, 'e', { name: 'E', count: 13, positive: true }),
-]
-
-// to have valid 'modulesInit' it must be evaluated only after all modules are created 
-export const init = {
-  ...modulesInit,
-  title: 'Counter + PositiveCoutner + Namespace'
-}
+export const modules = all.map(m => m.module)
+export const indexes = all.reduce((acc, m, idx) => { acc[m.slice] = idx; return acc }, {})
