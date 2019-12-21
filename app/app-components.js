8d8f21ae5e41e715dcb5c26a43a212b793328265
mshgh <36337240+mshgh@users.noreply.github.com>
2019-12-21 23:34:39 +0100

    Cleaning MultiCounter implementation

--- app/app-components.js
+++ app/app-components.js
@@ -8,7 +8,7 @@ export const WelcomeMessage = ({ navigateToCounters, navigateToSettings }) => Di
 export const Counters = (
   {
     actions: { [module.a]: a, [module.b]: b, [module.c]: c, [module.d]: d, [module.e]: e },
-    views: { [module.a]: aViews, [module.b]: bViews, [module.c]: cViews, [module.d]: dViews, [module.e]: eViews, [module.multiCounter]: multiCounter }
+    views: { [module.a]: aViews, [module.b]: bViews, [module.c]: cViews, [module.d]: dViews, [module.e]: eViews, [module.multiCounter]: multiCounterViews }
   }
 ) =>
   Div(
@@ -17,12 +17,12 @@ export const Counters = (
     cViews.IncDec({ title: 'Counter C', incrementOther: d.increment, decrementOther: d.decrement }),
     dViews.IncDec({ title: 'Counter D', incrementOther: e.increment, decrementOther: e.decrement }),
     eViews.IncDec({ title: 'Counter E', incrementOther: a.increment, decrementOther: a.decrement }),
-    ...multiCounter.Counters()
+    ...multiCounterViews.Counters()
   )
 
-export const Settings = ({ [module.c]: cViews, [module.e]: eViews, [module.multiCounter]: multiCounter }) =>
+export const Settings = ({ [module.c]: cViews, [module.e]: eViews, [module.multiCounter]: multiCounterViews }) =>
   Div(
     cViews.Settings(),
     eViews.Settings(),
-    ...multiCounter.Settings()
+    ...multiCounterViews.Settings()
   )
--- app/modules/multi-counter.js
+++ app/modules/multi-counter.js
@@ -1,32 +1,49 @@
-import { module } from '../../lib/modules.js'
+import { module, ns } from '../../lib/modules.js'
 import Counter from './counter/index.js'
 import PositiveCounter from './positive-counter/index.js'
 
 export default function MultiCounter(slice, { } = {}) {
 
   const counters = []
-  const init = []
+  let seq = -1
 
-  const [multiCounter] = module(slice, {
-    init,
-    views: state => ({
-      Counters: () => state.map((c, idx) => counters[idx].views(c).IncDec()),
-      Settings: () => state.map((c, idx) => counters[idx].views(c)).filter(v => v.Settings !== undefined).map(v => v.Settings())
-    })
+  const [multiCounter, child] = module(slice, {
+    views(slice) {
+      return function (state) {
+
+        function positiveCounter(module) {
+          return slice[module.id].type === 'positiveCounter'
+        }
+
+        function views(filter = _ => true) {
+          return counters.filter(filter).map(c => c.module.views(state))
+        }
+
+        return {
+          Counters() { return views().map(v => v.IncDec()) },
+          Settings() { return views(positiveCounter).map(v => v.Settings()) }
+        }
+      }
+    }
   })
 
   function add({ name, count, positive }) {
-    const counter = positive === undefined
-      ? Counter('counter', { name, count })
-      : PositiveCounter('positiveCounter', { name, count, positive })
+    const id = `m${++seq}`
+    const module = positive === undefined
+      ? ns(child(id)).add(Counter, 'module', { name, count })
+      : ns(child(id)).add(PositiveCounter, 'module', { name, count, positive })
 
-    counters.push(counter)
-    init.push(counter.init)
+    module.init.type = positive === undefined ? 'counter' : 'positiveCounter'
+    module.init.props = arguments[0]
+    counters.push({ id, module })
   }
 
   return {
     init: multiCounter.init,
-    views: multiCounter.views,
+    actions: multiCounter.actions,
+    views(state) {
+      return multiCounter.views(state)(state)
+    },
     add
   }
 }
