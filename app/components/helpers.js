df6221141771889c8925976add891c0e272f5754
mshgh <36337240+mshgh@users.noreply.github.com>
2020-01-04 10:57:29 +0100

    Added helper components

--- app/components/helpers.js
+++ app/components/helpers.js
@@ -14,7 +14,7 @@ export const ButtonLink = (label, onclick) => h('button', { onclick }, label)
 
 export const ButtonLinkDisabled = label => h('button', { disabled: true }, label)
 
-export const ButtonDelete = (onclick) => h('button', { onclick, title: 'delete', style: { 'margin-top': '6px' } }, 'x')
+export const ButtonDelete = (onclick) => h('button', { onclick, title: 'delete', style: { 'margin-top': '6px' } }, '\u2715')
 
 export const TextLink = (label, onclick) => h('span', { style: { cursor: 'pointer', 'text-decoration': 'underline' }, onclick }, label)
 
@@ -34,3 +34,7 @@ export const FloatLeft = (content) => h('div', {}, [
   ...content.map(c => h('div', { style: { float: 'left' } }, c)),
   h('div', { style: { clear: 'both' } })
 ])
+
+export const ON = () => h('span', { style: { color: 'green', 'font-weight': 'bold' } }, '\u2713')
+
+export const OFF = () => h('span', { style: { color: 'red', 'font-weight': 'bold' } }, '\u2715')
