e857c57135de69fa7ccde21fd81c87bf6dd012aa
mshgh <36337240+mshgh@users.noreply.github.com>
2023-11-21 10:21:10 +0100

    [WIP] build
    - removed support for build level props for actions and views

--- app/lib/hyperapp/msh/build.js
+++ app/lib/hyperapp/msh/build.js
@@ -1,10 +1,14 @@
-export const build = (operations, { focus, namespace, buildDi, asValue } = {}) => {
+export const build = (operations, { focus, namespace, buildDi } = {}) => {
   let mainView, currentState
   const focuses = [], ROOT_NS = 'root', namespaces = [ROOT_NS], views = [{ [ROOT_NS]: {} }], actions = [{ [ROOT_NS]: {} }]
 
   const isA = a => Array.isArray(a), isF = f => typeof f === 'function'
-  const toValue = (props, value = props?.[asValue]) => typeof value === 'undefined' ? props : value
-  const getKey = (focus, props) => focus[0] === '@' ? props?.[focus.slice(1)] : focus
+  const getPropsKey = (focus, props) => {
+    const key = props.reduce((key, props) => !key ? props?.[focus] : key, undefined)
+    if (typeof key === 'undefined') throw new Error(`Key ${focus} not found in props`)
+    return key
+  }
+  const getKey = (focus, ...props) => focus[0] === '@' ? getPropsKey(focus.slice(1), props) : focus
   const getName = (fnName, name) => typeof name === 'undefined' ? fnName : name
   const setCurrent = (stack, name, value) => !name ? stack[0][namespaces[0]] = value : stack[0][namespaces[0]][name] = value
   const pushViewsActions = name => {
@@ -18,18 +22,18 @@ export const build = (operations, { focus, namespace, buildDi, asValue } = {}) =
     if (count > (stack.length - keep)) throw new Error(`Cannot pop '${count}' item(s) from stack with depth '${stack.length} and keep '${keep}' item(s)`)
     stack.splice(0, count)
   })
-  
-  const focusAction = (actionFocus, action, { curriedDi, di, props: buildProps } = {}) => {
+
+  const focusAction = (actionFocus, action, { curriedDi, di } = {}) => {
     const inject = di && di(actions[0][namespaces[0]])
     if (curriedDi) action = action(inject)
-    return (state, actionProps) => {
-      const props = actionProps?.constructor === Object ? { ...buildProps, ...actionProps } : actionProps
+    return (state, props) => {
+      if (inject && !curriedDi && props?.constructor !== Object) throw new Error(`Cannot inject dependencies into scalar props`)
       const subStates = actionFocus.reduce((acc, focus) => {
         acc.unshift(!acc.length ? [state || {}, focus] : [acc[0][0][acc[0][1]] || (focus[0] === '@' ? [] : {}), getKey(focus, props)])
         return acc
       }, [])
       const subState = !subStates.length ? state : subStates[0][0][subStates[0][1]]
-      const result = action(subState, inject && !curriedDi && props !== actionProps ? { ...inject, ...props } : toValue(props))
+      const result = action(subState, inject && !curriedDi ? { ...inject, ...props } : props)
       const newSubState = isA(result) ? result[0] : result
       if (isF(newSubState)) return result
       const newState = newSubState === subState ? state : subStates.reduce((newState, [parent, key]) => {
@@ -40,13 +44,12 @@ export const build = (operations, { focus, namespace, buildDi, asValue } = {}) =
     }
   }
 
-  const focusView = (viewFocus, view, { curriedDi, di, subState = s => s, props: buildProps } = {}) => {
+  const focusView = (viewFocus, view, { curriedDi, di, subState } = {}) => {
     const inject = di && di({ ...buildDi, views: views[0][namespaces[0]], actions: actions[0][namespaces[0]] })
     if (curriedDi) view = view(inject)
-    return (viewProps, children) => {
-      const props = { ...buildProps, ...viewProps }
+    return (props, children) => {
       const state = viewFocus.reduce((acc, focus) => acc?.[getKey(focus, props)], currentState)
-      return view(subState(state), curriedDi || !inject ? props : { ...inject, ...props }, children)
+      return view(!subState ? state : subState(state), inject && !curriedDi ? { ...inject, ...props } : props, children)
     }
   }
 
